815a63c7fd67bfedcd12db0c856d99d8a540529d
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-28 09:12:52 -0400

    Updated settings for secure transport

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 const config = require('../app.config')
 
-const socket = io.connect(config.socket)
+const socket = io.connect(config.socket, { secure: true })
 
 export default socket
