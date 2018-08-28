eb82cf64e4faba6f039bb8208a699c0bc501f521
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-28 13:53:49 -0400

    reconfigure socket settings

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,8 +1,6 @@
 import io from 'socket.io-client'
 const config = require('../app.config')
 
-const socket = io.connect(config.socket, {
-	reconnection: true,
-})
+const socket = io(config.socket, { reconnection: true, })
 
 export default socket
