ffc9a95ff09cb80b59d53cf2a6d168b8db3573b9
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-28 16:18:52 -0400

    adding wss might work

--- app.config.js
+++ app.config.js
@@ -35,7 +35,7 @@ let config = {
 if (process.env.NODE_ENV == 'production') {
 	config.auth.cookie.options.secure = true
 	config.axios.baseURL = process.env.BASE_URL
-	config.socket = `wss://${ process.env.BASE_URL }`
+	config.socket = 'wss://https://example-cb-api.herokuapp.com/'
 }
 
 module.exports = config
