f1567fb2fecb4c94a88d1c27c7c071dd75b8c334
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-28 11:52:27 -0400

    Updated settings for socket

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,9 @@
 import io from 'socket.io-client'
 const config = require('../app.config')
 
-const socket = io.connect(config.socket, { secure: true })
+const socket = io.connect(config.socket, {
+	forceNew: true,
+	secure: true
+})
 
 export default socket
