10de3deb397510e323aab02ca921c637ec5ad466
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-28 16:26:20 -0400

    hard coded address?

--- app.config.js
+++ app.config.js
@@ -35,7 +35,7 @@ let config = {
 if (process.env.NODE_ENV == 'production') {
 	config.auth.cookie.options.secure = true
 	config.axios.baseURL = process.env.BASE_URL
-	config.socket = 'ws://https://example-cb-api.herokuapp.com/'
+	config.socket = 'https://example-cb-api.herokuapp.com/'
 }
 
 module.exports = config
